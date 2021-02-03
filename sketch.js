9d470b8a9470a555074914ba9c5dc540e75bbfb6
Spandan <74659727+Spandan-maker@users.noreply.github.com>
2021-02-03 17:48:26 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -2,29 +2,24 @@ var canvas, backgroundImage;
 
 var gameState = 0;
 var playerCount;
-
+var allPlayers;
 var database;
 
 var form, player, game;
-var allPlayers
-var distance = 0;
+
 
 function setup(){
   canvas = createCanvas(400,400);
   database = firebase.database();
-
   game = new Game();
   game.getState();
   game.start();
 }
 
-
 function draw(){
-
   if(playerCount === 4){
-    game.update(1)
-  }
-  
+    game.update(1);
+  } 
   if(gameState === 1){
     clear();
     game.play();
